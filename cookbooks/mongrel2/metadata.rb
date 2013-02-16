2c4cabfee86fffbbec02ab452f0f92626baac2a2
Xavier Lange <xrlange@gmail.com>
2013-02-16 01:25:55 -0800

    Removing mongrel2 real quick... goofed

--- cookbooks/mongrel2/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Thomas Rampelberg"
-maintainer_email "thomas@saunter.org"
-license          "Apache 2.0"
-description      "Installs/Configures mongrel2"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.5"
-
-%w( build-essential ).each{|cookbook| depends cookbook }
-# depends "daemontools"
