bee0dc9c22483727cfcd7905b4c85160e8f8852c
Xavier Lange <xrlange@gmail.com>
2013-02-21 00:18:33 -0800

    Whoa, lots of fixes for this LWRP to actually work

--- cookbooks/mercurial/providers/default.rb
+++ cookbooks/mercurial/providers/default.rb
@@ -1,41 +1,41 @@
 action :sync do
   ::File.delete(hgup_file) if ::File.exist?(hgup_file)
+  execute "set ownership" do
+    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
+    action :nothing
+  end
+  execute "set permissions" do
+    only_if { new_resource.mode }
+    command "chmod -R #{new_resource.mode} #{new_resource.path}"
+  end
   execute "sync repository #{new_resource.path}" do
     not_if "hg identify #{new_resource.path}"
     command "hg clone --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository} #{new_resource.path} && touch #{hgup_file}"
     creates hgup_file
-    notifies :run, "execute[set ownership]"
-    notifies :run, "execute[set permissions]"
+    notifies :run, resources(:execute => "set ownership")
+    notifies :run, resources(:execute => "set permissions")
   end
-  execute "check incoming changes" do
-    command "hg incoming --rev #{new_resource.reference} #{hg_connection_command}  #{new_resource.repository} && touch #{hgup_file} || true"
+  execute "update" do
+    command "hg update"
     cwd new_resource.path
-    creates hgup_file
-    notifies :run, "execute[pull]"
+    action :nothing
+    notifies :run, resources(:execute => "set ownership")
+    notifies :run, resources(:execute => "set permissions")
   end
   execute "pull" do
     command "hg pull --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository}"
     cwd new_resource.path
     only_if { ::File.exist?(hgup_file) }
     action :nothing
-    notifies :run, "execute[update]"
+    notifies :run, resources(:execute => "update")
   end
-  execute "update" do
-    command "hg update"
+  execute "check incoming changes" do
+    command "hg incoming --rev #{new_resource.reference} #{hg_connection_command}  #{new_resource.repository} && touch #{hgup_file} || true"
     cwd new_resource.path
-    action :nothing
-    notifies :run, "execute[set ownership]"
-    notifies :run, "execute[set permissions]"
+    creates hgup_file
+    notifies :run, resources(:execute => "pull")
   end
-  execute "set ownership" do
-    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
-    action :nothing
-  end
-  if new_resource.mode then
-    execute "set permissions" do
-      command "chmod -R #{new_resource.mode} #{new_resource.path}"
-    end
-  end
+
   if ::File.exist?(hgup_file)
     new_resource.updated_by_last_action(true)
     ::File.delete(hgup_file)
